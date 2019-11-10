a58599a278143e6b85f48c781b848b31dedcf627
Nikita <yakuri2006@gmail.com>
2019-11-10 17:03:42 +0300

    Cloned files from old repo

---
+++ EljurSched.py
@@ -0,0 +1,97 @@
+from googleapiclient.discovery import build
+from login import google_login, eljur_login
+from colored import fg, stylize, attr
+import requests as rq
+import datetime
+
+
+primary_calendar_id = "yakuri2006@gmail.com"
+eljur_calendar_id = "6sorgqebejho8m0cpof065eja8@group.calendar.google.com"
+SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
+devkey = "9235e26e80ac2c509c48fe62db23642c"
+include_non_academ = "true"
+student_id = "6461"
+today = datetime.datetime.now().__str__()[:10]
+print(today)
+lessons = []
+time_style = fg("green") + attr("bold")
+separator_style = fg("medium_purple_1") + attr("bold")
+nonacadem_style = fg("red")
+separator = stylize("::", separator_style)
+
+class Lesson_Event:
+    def __init__(self, date, time, TZ="Europe/Moscow"):
+        self.tz = "+03:00"
+        self.date = date
+        self.starttime, self.endtime = time.split("_")
+
+    def __date_to_google_format(self):
+        self.date.replace("-", "")
+        self.date += "T"
+
+    def start_google_format(self):
+        self.__date_to_google_format()
+        return self.date + self.starttime + self.tz
+
+    def end_google_format(self):
+        self.__date_to_google_format()
+        return self.date + self.endtime + self.tz
+
+def output_time(time):
+    start, end = time.split("_")
+    start = start[:5]
+    end = end[:5]
+    return start + "-" + end
+
+token = eljur_login()
+creds = google_login()
+
+diary_params = {"student": student_id, "days": "20191104-20191109", "rings": include_non_academ,
+                "devkey": devkey, "out_format": "json",
+                "auth_token": token, "vendor": "markbook"}
+
+
+time_sort = {10: "08:30:00_09:10:00", 20: "09:30:00_10:10:00", 30: "10:20:00_11:00:00", 40: "11:10:00_11:50:00",
+             50: "12:00:00_12:40:00", 60: "13:30:00_14:10:00", 70: "14:20:00_15:00:00", 80: "15:10:00_15:50:00",
+             90: "16:20:00_17:00:00", 100: "17:10:00_17:50:00", 110: "18:00:00_18:40:00"}
+
+# service = build('calendar', 'v3', credentials=creds)
+# calendar = service.calendars().get(calendarId=primary_calendar_id).execute()
+# events = service.events().list(calendarId=primary_calendar_id).execute()
+
+diary = rq.get("https://markbook.eljur.ru/apiv3/getschedule", params=diary_params).json()['response']
+if diary["error"] is not None:
+    print("Error: " + diary["error"])
+    raise ValueError
+schedule = diary['result']['students'][str(student_id)]
+for d in schedule["days"]:
+    d = schedule["days"][d]
+    current_schedule = d
+    print("\n" + d["title"] + " \n")
+    for i in range(len(current_schedule["items"]) + 1):
+        if i == 0:
+            continue
+        if not current_schedule["items"].get(str(i)):
+            print("     {0} {1} Окно!".format(stylize(output_time(time_sort[i * 10]), time_style), separator))
+            continue
+        i = current_schedule["items"][str(i)]
+        if not i.get("starttime"):
+            if i["room"] != "":
+                print("     {0} {1} {2} в кабинете {3}".format(stylize(output_time(time_sort[int(i["sort"])]), time_style), separator, i["name"], i["room"]))
+                # print(Lesson_Event(today, time_sort[int(i["sort"])]).start_google_format())
+            else:
+                print("     {0} {1} {2}".format(stylize(output_time(time_sort[int(i["sort"])]), time_style), separator, i["name"]))
+        else:
+            if i["room"] != "":
+                print("     {0} {1} {2} в кабинете {3}".format(stylize(output_time(i["starttime"] + "_" + i["endtime"]), time_style), separator, i["name"], i["room"]))
+                # print(Lesson_Event(today, time_sort[int(i["sort"])]).start_google_format())
+            else:
+                print("     {0} {1} {2}".format(stylize(output_time(i["starttime"] + "_" + i["endtime"]), time_style), separator, i["name"]))
+    if current_schedule.get("items_extday"):
+        print(stylize("\n Внеакадем:\n", nonacadem_style))
+        for v in range(len(current_schedule["items_extday"])):
+            v = current_schedule["items_extday"][v]
+            print("     {0} {1} {2}".format(stylize(output_time(v["starttime"] + "_" + v["endtime"]), time_style), separator, v["name"]))
+    else:
+        print(stylize("\n Внеакадем отсутствует", nonacadem_style))
+
---
+++ login.py
@@ -0,0 +1,68 @@
+import pickle
+import os.path
+import PyInquirer as pq
+import json
+import requests as rq
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+credentials_eljur = open('credentials_eljur.txt')
+SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
+
+
+def google_login():
+    creds = None
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+
+    service = build('calendar', 'v3', credentials=creds)
+
+    return service
+
+
+def eljur_login():
+    with open("credentials.json", "r") as creds:
+        logged_users = json.load(creds)
+    eljur_auth_choice = [{"type": "list", "message": "Select your username",
+                             "name": "auth_choice", "default": 1,
+                              "choices": []}]
+    eljur_login_input = [{"type": "input", "name": "login", "message":"Enter your login:"}]
+    eljur_password_input = [{"type": "password", "name": "password", "message":"Enter your password:"}]
+    usernames = list(logged_users.keys())
+    for i in range(len(usernames)):
+        eljur_auth_choice[0]["choices"].append({"name": usernames[i]})
+    eljur_auth_choice[0]["choices"].append({"name": "Add new account"})
+    answer = {}
+    while not answer.get("auth_choice"):
+        answer = pq.prompt(eljur_auth_choice)
+    login = answer["auth_choice"]
+    devkey = "9235e26e80ac2c509c48fe62db23642c"
+    if login not in logged_users.keys():
+        login = pq.prompt(eljur_login_input)['login']
+        password = pq.prompt(eljur_password_input)['password']
+        auth_params = {"devkey": devkey, "out_format": "json", "vendor": "markbook", "login": login,
+                       "password": password}
+        auth_response = rq.get("https://markbook.eljur.ru/apiv3/auth", params=auth_params).json()
+        if auth_response["response"]["state"] != 200 or "200":
+            print("Error: " + auth_response["response"]["error"])
+        token = auth_response['response']['result']['token']
+        with open("credentials.json", "w") as write_creds:
+            json.dump({login: token}, write_creds)
+        return token
+    else:
+        token = logged_users[login]
+        return token
+
+def get_rules(token):
+    
